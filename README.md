ec69dd861716322dff2b8047f4e2c15d9d44e5bd
Nick Freear <nfreear@yahoo.co.uk>
2021-05-28 08:45:37 +0100

    Plugin to add `Content-Security-Policy` header (#2)
    * Plus, add `integrity` attrbutes to `<script>`
    * Initial README docs

---
+++ README.md
@@ -0,0 +1,28 @@
+
+# survey-bot
+
+A speech-enabled Chat-bot.
+
+Get the user to answer questions and talk about themselves!
+
+Uses:
+
+* [Node.js][]
+* [NLP.js][]
+* [Webchat.js][]
+* [MS Speech SDK][speech-sdk]
+
+---
+<!-- © 2021 Nick Freear. -->
+
+[node.js]: https://nodejs.org/
+  "Node.js® is a JavaScript runtime built on Chrome's V8 JavaScript engine."
+[nlp.js]: https://github.com/axa-group/nlp.js
+  "'NLP.js' is a general natural language utility for nodejs"
+[webchat.js]: https://github.com/microsoft/BotFramework-WebChat/releases/tag/v4.13.0
+  "Bot Framework Web Chat Javascript component"
+[speech-sdk]: https://github.com/microsoft/cognitive-services-speech-sdk-js
+  "Microsoft Cognitive Services Speech SDK for JavaScript"
+[speech-cdn]: https://jsdelivr.com/package/npm/microsoft-cognitiveservices-speech-sdk?path=distrib%2Fbrowser
+[speech-raw]: https://aka.ms/csspeech/jsbrowserpackageraw
+  "Via: github.com/Azure-Samples/cognitive-services-speech-sdk/.."
---
+++ src/plugins/security-header-middleware.js
@@ -0,0 +1,106 @@
+/**
+ * Middleware plugin to set a 'Content-Security-Policy' HTTP header.
+ *
+ * @see https://content-security-policy.com/examples/express-js/
+ * @see https://helmetjs.github.io/
+ * @see https://csp-evaluator.withgoogle.com/
+ *
+ * @copyright © Nick Freear, 27-May-2021.
+ */
+
+const { defaultContainer, Clonable } = require('@nlpjs/core');
+
+// Sub-resource integrity SRI hashes.
+const HASH = {
+  // https://cdn.jsdelivr.net
+  speechSdk: 'sha256-psfl63lul/bQX9y0lE9MOSmbYDhMa0vF5hp5MKalgJs=',
+  // https://cdn.botframework.com
+  webchat: 'sha384-Xm4uX5+AacO2cl3ffEKrytyC+iHBSsNvXrJTTzodBhgkiuxIS0YWHGq1KFcf0Rr+'
+}
+
+const SECURITY_POLICY = [
+  "default-src 'self'",
+  "connect-src 'self' https://*.tts.speech.microsoft.com wss://*.stt.speech.microsoft.com",
+  "frame-ancestors 'self' https://*.nquire.org.uk https://*.onlinesurveys.ac.uk",
+  "img-src 'self' blob: data: ",
+  "object-src 'none'",
+  `script-src 'self' '${HASH.webchat}' '${HASH.speechSdk}' https://nfreear.github.io`, // https://unpkg.com;
+  "style-src 'self' 'unsafe-inline'",
+  "worker-src 'self' blob: "
+];
+
+const PAGE_URL_REGEX = /^\/(index\.html)?(\?.+)?$/;
+
+class SecurityHeaderMiddleware extends Clonable {
+  constructor (settings = {}, container) {
+    super({
+      settings: {},
+      container: settings.container || container || defaultContainer
+    },
+    container
+    );
+
+    this.name = 'securityHeaderMiddleware';
+
+    const { API_ENABLE } = process.env;
+
+    this.apiEnable = API_ENABLE;
+
+    console.log('apiEnable:', API_ENABLE);
+
+    this.initialize();
+  }
+
+  run (input) {
+    this.logger.info(`${this.name}.run() ~ NO-OP!`);
+
+    return input;
+  }
+
+  initialize () {
+    const apiServer = this.container.get('api-server'); // .app;
+
+    this.logger.info(`${this.name}.initialize()`);
+
+    if (!apiServer) {
+      throw new Error('No api-server found');
+    }
+
+    if (!this.apiEnable) {
+      return;
+    }
+
+    apiServer.plugins.push((req, res, next) => {
+      if (PAGE_URL_REGEX.test(req.url)) {
+        res.set({
+          'X-My-Header': `${this.name}.via.plugins.push`,
+          'Content-Security-Policy': SECURITY_POLICY.join('; ') + ';',
+        });
+
+        this.logger.info(`CSP header sent.`);
+      }
+
+      next();
+    });
+
+    console.log(apiServer);
+
+    return;
+
+    /* this.app = apiServer.app;
+
+    this.app.use((req, res, next) => {
+
+      res.set({
+        'X-My-Header': `${this.name}.via.use`,
+        'Content-Security-Policy': POLICY.join('; ') + ';',
+      });
+
+      // console.log('CSP header set?');
+
+      next();
+    }); */
+  }
+}
+
+module.exports = SecurityHeaderMiddleware;
