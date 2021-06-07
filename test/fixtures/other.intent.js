c6df9405351b7efaf75508ef684a8c1b5e6a8a83
Nick Freear <nfreear@yahoo.co.uk>
2021-06-07 18:08:02 +0100

    fix: Unit test for `survey-bot` ~ fix `other.intent` fixture (#3)

--- test/fixtures/other.intent.js
+++ test/fixtures/other.intent.js
@@ -6,11 +6,17 @@
 module.exports = [
   {
     message: 'Do you have a stutter?',
+    from: { id: 'nick', name: 'Nick User', role: 'user' },
+    activity: { channelData: {} },
+
     intent: 'do.you.stammer',
     score: 0.888,
     answer: undefined
   }, {
     message: 'I need help with my stammer',
+    from: { id: 'nick', name: 'Nick User', role: 'user' },
+    activity: { channelData: {} },
+
     intent: 'i.need.help.stammer',
     score: 0.888
   }
